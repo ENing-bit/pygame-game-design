a4f789e78c8fdda8a9b807ff4e09453f4e8164cd
GanX <2423855310@qq.com>
2025-06-15 11:35:22 +0800

    feat(ui):添加了手动重新开始的Button

--- gameObject/ui/parse_game_ui_canva.py
+++ gameObject/ui/parse_game_ui_canva.py
@@ -11,6 +11,13 @@ class ParseGameUICanvas(UICanvas):
 
         self.game_map_manager = game_map_manager
 
+        self.restart_button = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((500, 500), (600, 50)),
+            text="Press To Restart",
+            manager=self.ui_manager,
+            object_id="#restart_button",
+        )
+
         self.parse_label = pygame_gui.elements.UILabel(
             relative_rect=pygame.Rect((500, 600), (600, 50)),
             text="Parse Game",
@@ -18,6 +25,7 @@ class ParseGameUICanvas(UICanvas):
             object_id="#parse_label",
         )
 
+        self.restart_button.hide()
         self.parse_label.hide()
 
     def init(self, sender, **kwargs):
@@ -28,19 +36,30 @@ class ParseGameUICanvas(UICanvas):
     
     def event(self, sender, **kwargs):
         super().event(sender, **kwargs)
-        if kwargs.get("event", None).type == pygame.KEYDOWN:
-            if kwargs.get("event", None).key == pygame.K_ESCAPE:
+        event = kwargs.get("event", None)
+        if event is None:
+            return
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
                 Game._instance.game_pause(self)
-    
+        # 监听按钮点击事件
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == self.restart_button:
+                Game._instance.game_resume(self)
+
+        
     def draw(self, sender, **kwargs):
         super().draw(sender, **kwargs)
     
     def on_game_start(self, sender):
         self.parse_label.hide()
+        self.restart_button.hide()
 
     def on_game_pause(self, sender):
         self.parse_label.show()
+        self.restart_button.show()
 
     def on_game_resume(self, sender):
         self.parse_label.hide()
-    
+        self.restart_button.hide()
+        
